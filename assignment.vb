e83254e2e62ee7135bc7143bfd7e140f03567499
Simon Ugorji <ugorji757@gmail.com>
2023-04-07 06:57:45 +0100

    Added for..next loop and array demo

---
+++ assignment.vb
@@ -0,0 +1,21 @@
+Module arrayApl
+    Sub Main()
+
+        Dim even(9) As Integer 'Create array that will store Integers. This array will hold 10 even numbers
+    
+        For i As Integer = 1 To 10 'Create a counter i and then Loop from 1 - 10
+
+            even(i - 1) = 2 * i 'Multiply the counter by 2 and store to the array index - 1 
+
+        Next i 'Increment the counter i
+        
+        'Loop through the even numbers array
+        For j as Integer = 0 To (even.length - 1) 'Create a counter j and then loop from 0 - 9
+
+            Console.WriteLine(even(j)) 'Print the value of the even array at that current index
+
+        Next j 'increment the counter j
+    
+    End Sub
+End Module
+
